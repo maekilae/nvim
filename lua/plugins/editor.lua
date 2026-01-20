6c25e08a4f6c57ab358c0f88fe50e9cac60acd1b
Emph <marcus@emph.dev>
2026-01-20 22:57:09 +0100

    mini plugins added

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,46 +1,66 @@
 return {
 	{
 		"https://github.com/nvim-telescope/telescope.nvim",
-		tag = "*", 
+		tag = "*",
 		deps = {
-			{'https://github.com/nvim-lua/plenary.nvim'},
-			{'https://github.com/nvim-telescope/telescope-fzf-native.nvim', build = 'make'}
+			{ 'https://github.com/nvim-lua/plenary.nvim' },
+			{ 'https://github.com/nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
 		},
-		opts={
+		opts = {
 
 		},
 		keys = {
-      { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Telescope find files" },
-      { "<leader>fg", "<cmd>Telescope live_grep<cr>",  desc = "Telescope live grep" },
-      { "<leader>fb", "<cmd>Telescope buffers<cr>",    desc = "Telescope buffers" },
-      { "<leader>fh", "<cmd>Telescope help_tags<cr>",  desc = "Telescope help tags" },
-    },
+			{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Telescope find files" },
+			{ "<leader>fg", "<cmd>Telescope live_grep<cr>",  desc = "Telescope live grep" },
+			{ "<leader>fb", "<cmd>Telescope buffers<cr>",    desc = "Telescope buffers" },
+			{ "<leader>fh", "<cmd>Telescope help_tags<cr>",  desc = "Telescope help tags" },
+		},
 	},
 	{
-  "https://github.com/mikavilpas/yazi.nvim",
-  tag = "*", -- use the latest stable version
-  --event = "VeryLazy",
-  deps = {
-  },
-  keys = {
-    -- 👇 in this section, choose your own keymappings!
-    {
-      "<leader>n",
-      "<cmd>Yazi<cr>",
-      mode = { "n", "v" },
-      desc = "Open yazi at the current file",
-    },
-    {
-      -- Open in the current working directory
-      "<leader>cw",
-      "<cmd>Yazi cwd<cr>",
-      desc = "Open the file manager in nvim's working directory",
-    },
-    {
-      "<c-up>",
-      "<cmd>Yazi toggle<cr>",
-      desc = "Resume the last yazi session",
-    },
-  }
-  },
+		"https://github.com/mikavilpas/yazi.nvim",
+		tag = "*", -- use the latest stable version
+		--event = "VeryLazy",
+		deps = {
+		},
+		keys = {
+			-- 👇 in this section, choose your own keymappings!
+			{
+				"<leader>n",
+				"<cmd>Yazi<cr>",
+				mode = { "n", "v" },
+				desc = "Open yazi at the current file",
+			},
+			{
+				-- Open in the current working directory
+				"<leader>cw",
+				"<cmd>Yazi cwd<cr>",
+				desc = "Open the file manager in nvim's working directory",
+			},
+			{
+				"<c-up>",
+				"<cmd>Yazi toggle<cr>",
+				desc = "Resume the last yazi session",
+			},
+		}
+	},
+	{
+		'folke/which-key.nvim',
+		name = 'which-key',
+		opts = {
+			spec = {
+				mode = { "n", "x" },
+				{ "<leader>c",  group = "code" },
+				{ "<leader>d",  group = "debug" },
+				{ "<leader>dp", group = "profiler" },
+				{ "<leader>f",  group = "file/find" },
+				{ "<leader>s",  group = "search" },
+				{ "[",          group = "prev" },
+				{ "]",          group = "next" },
+				{ "g",          group = "goto" },
+				{ "gs",         group = "surround" },
+				{ "gc",         group = "comment" },
+				{ "z",          group = "fold" },
+			}
+		},
+	},
 }
